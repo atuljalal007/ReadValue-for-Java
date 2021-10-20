cd0931b0e33ff0e931695933d2db085cdc493af5
Atul J <atul.jalal@litmus7.com>
2021-10-20 23:14:08 +0530

    message

---
+++ ReadValue - Copy/src/readvalue.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class readvalue {
+public static void main(String []args)
+{
+	System.out.println("\n enter a number");
+	Scanner sc =new Scanner(System.in);
+	int no = sc.nextInt();
+	System.out.println("\n enter a double");
+	double dob = sc.nextDouble();
+	System.out.println("\n enter a string");
+	String str =sc.next();
+	System.out.println("\n enter a float");
+	float strf =sc.nextFloat();
+}
+}
